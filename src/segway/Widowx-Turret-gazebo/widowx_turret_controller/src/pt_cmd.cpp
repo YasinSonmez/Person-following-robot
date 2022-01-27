24f270863245fdc354a70ef1fc54166e2b6272ab
ysoenmez <yasin.soenmez@gitlab.tuebingen.mpg.de>
2022-01-27 20:32:55 +0300

    Efficient human tracking with head

--- src/segway/Widowx-Turret-gazebo/widowx_turret_controller/src/pt_cmd.cpp
+++ src/segway/Widowx-Turret-gazebo/widowx_turret_controller/src/pt_cmd.cpp
@@ -1,9 +1,13 @@
 #include "ros/ros.h"
+#include "std_msgs/Int16.h"
 #include "std_msgs/Float64.h"
+#include "nav_msgs/Odometry.h"
 #include <iostream>
 #include <sstream>
 #include "control_msgs/JointControllerState.h"
 #include <cmath>
+#include <tf/tf.h>
+using namespace std;
 
 #define CAM_WIDTH 640
 #define CAM_HEIGHT 480
@@ -12,93 +16,120 @@
 #define RY_LIM PI / 3.6
 #define REFLECT_AXIS_X 1
 #define REFLECT_AXIS_Y 1
-#define E_SS_LIM 0.01
+#define E_SS_LIM 0.15
 
 ros::Publisher panRefPub;
 ros::Publisher tiltRefPub;
-float current_rx = 0, current_ry = 0;
-bool is_pan_moving = false, is_tilt_moving = false;
+float prev_rx = 0, current_rx = 0, current_ry = 0, offset = 0, angle_added = 0,
+      prev_angle_added = 0, yaw = 0, target_difference = 0, angle_increment = 0.06;
+// bool is_pan_moving = false, is_tilt_moving = false;
+int is_human_detected = 0, dont_increase_count = 0, lost_count = 0;
+nav_msgs::Odometry odom;
 
-float calcRef(float target, float *current_r, int maxRes, float r_lim, bool is_reflected){
+float mod(float a, float N) { return a - N * floor(a / N); } // return in range [0, N)
+
+float calcRef(float target, int maxRes, float r_lim, bool is_reflected)
+{
     float center = maxRes / 2;
     float diff = target - center;
-    float new_r = (diff + center) / (maxRes) * r_lim - r_lim / 2;
-    if (is_reflected){
+    float new_r = (diff + center) / (maxRes)*r_lim - r_lim / 2;
+    if (is_reflected)
+    {
         new_r = -new_r;
     }
-    new_r += *current_r;
-    *current_r = new_r;
     return new_r;
 }
 
-void targetXCb(const std_msgs::Float64::ConstPtr& targetX){
-    if (is_pan_moving){
-        ROS_INFO("Pan is busy");
-        return;
-    }
-    if (targetX->data == 0){
-        current_rx = 0;
-    }
-    else {
-        current_rx = calcRef(targetX->data, &current_rx, CAM_WIDTH, RX_LIM, REFLECT_AXIS_X);
-    }
-
-    std_msgs::Float64 panRef;
-    panRef.data = current_rx;
-    panRefPub.publish(panRef);
+void targetXCb(const std_msgs::Int16::ConstPtr &targetX)
+{
+    is_human_detected = 200;
 }
-
-void targetYCb(const std_msgs::Float64::ConstPtr& targetY){
-    if (is_tilt_moving){
-        ROS_INFO("Tilt is busy");
-        return;
-    }
-    if (targetY->data == 0){
-        current_ry = 0;
-    }
-    else {
-        current_ry = calcRef(targetY->data, &current_ry, CAM_HEIGHT, RY_LIM, REFLECT_AXIS_Y);
-    }
-    std_msgs::Float64 tiltRef;
-    tiltRef.data = current_ry;
-    tiltRefPub.publish(tiltRef);
-}
-
-bool detect_movement(float r, float y, const std::string& name){
-    float err = r - y; 
-    if (std::abs(err) > E_SS_LIM){
-        ROS_INFO("%s is moving. Err: %f", name.c_str(), err);
+/*
+bool detect_movement(float r, float y, const string &name)
+{
+    float err = r - y;
+    if (abs(err) > E_SS_LIM)
+    {
+        // ROS_INFO("%s is moving. Err: %f", name.c_str(), err);
         return true;
     }
     return false;
 }
+*/
 
-void pan_state_cb(const control_msgs::JointControllerState::ConstPtr& panState){
-    is_pan_moving = detect_movement(current_rx, panState->process_value, "pan");
+void panStateCb(const control_msgs::JointControllerState::ConstPtr &panState)
+{
+    // is_pan_moving = detect_movement(current_rx, panState->process_value, "pan");
+    if (is_human_detected)
+        is_human_detected--;
 }
 
-void tilt_state_cb(const control_msgs::JointControllerState::ConstPtr& tiltState){
-    is_tilt_moving = detect_movement(current_ry, tiltState->process_value, "tilt");
+void odomCb(const nav_msgs::Odometry::ConstPtr &odom_msg)
+{
+    odom = *odom_msg;
+
+    // Get the yaw angle from quaternion
+    tf::Pose pose;
+    tf::poseMsgToTF(odom.pose.pose, pose);
+    yaw = -tf::getYaw(pose.getRotation());
 }
 
+void humanPoseCb(const geometry_msgs::PoseStamped::ConstPtr &human_pose)
+{
+    double focus_angle = atan2((human_pose->pose.position.y - odom.pose.pose.position.y),
+                               (human_pose->pose.position.x - odom.pose.pose.position.x));
+    std_msgs::Float64 panRef;
+    if (is_human_detected)
+    {
+        current_rx = mod((yaw + focus_angle + PI), (2 * PI)) - PI;
 
-int main(int argc, char **argv){
+        panRef.data = current_rx;
+        panRefPub.publish(panRef);
 
+        angle_added = 0;
+        prev_rx = current_rx;
+    }
+    else
+    {
+        // If turning 2*pi to catch up, dont increase angle for some time
+        if (abs(prev_rx - current_rx) > 0.1)
+            dont_increase_count = 40;
+        // Increase the angle with small increments to find the human
+        if (!dont_increase_count)
+            angle_added = mod((angle_added + angle_increment + PI), (2 * PI)) - PI;
+
+        prev_rx = current_rx;
+        current_rx = mod((yaw + focus_angle + angle_added + PI), (2 * PI)) - PI;
+        // If the head moves 2*pi degrees count as lost
+        if (prev_angle_added < 0 && angle_added > 0)
+        {
+            lost_count++;
+            cout << " Lost count: " << lost_count << endl;
+        }
+        prev_angle_added = angle_added;
+        panRef.data = current_rx;
+        panRefPub.publish(panRef);
+
+        if (dont_increase_count)
+            dont_increase_count--;
+    }
+}
+
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "pt_cmd");
 
     ros::NodeHandle nh;
-    
+
     panRefPub = nh.advertise<std_msgs::Float64>("/pan_controller/command", 1);
-    tiltRefPub = nh.advertise<std_msgs::Float64>("/tilt_controller/command", 1);
 
     ros::Subscriber targetXSub = nh.subscribe("targetX", 1, targetXCb);
-    ros::Subscriber targetYSub = nh.subscribe("targetY", 1, targetYCb);
+    ros::Subscriber odomSub = nh.subscribe("base_pose_ground_truth", 1, odomCb);
+    ros::Subscriber panStateSub = nh.subscribe("/pan_controller/state", 1, panStateCb);
+    ros::Subscriber humanPoseSub = nh.subscribe("human_pose", 1, humanPoseCb);
 
-    ros::Subscriber panStateSub = nh.subscribe("/pan_controller/state", 1, pan_state_cb);
-    ros::Subscriber tiltStateSub = nh.subscribe("/pan_controller/state", 1, tilt_state_cb);
-
-    while (true){
+    while (true)
+    {
         ros::spinOnce();
     }
-    
 }
