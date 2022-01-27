f82f004e9501993f186abd3f3ae8c7f4c2ec53f8
ysoenmez <yasin.soenmez@gitlab.tuebingen.mpg.de>
2022-01-27 20:30:48 +0300

    Message filter for synchronization

--- src/yolo_pp/src/yolo_pp.cpp
+++ src/yolo_pp/src/yolo_pp.cpp
@@ -1,3 +1,12 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <darknet_ros_msgs/BoundingBox.h>
+#include <darknet_ros_msgs/BoundingBoxes.h>
+
 #include "geometry_msgs/Point.h"
 #include "opencv2/objdetect/objdetect.hpp"
 #include "std_msgs/Int16.h"
@@ -8,7 +17,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/video/tracking.hpp>
-#include <ros/ros.h>
 #include <sensor_msgs/image_encodings.h>
 
 #include <darknet_ros_msgs/BoundingBox.h>
@@ -18,20 +26,22 @@
 #include <sys/time.h>
 #include <vector>
 
-//#include <apes_base/baseMsg.h>
 #include "kalman_filter.h"
 #include <geometry_msgs/PoseStamped.h>
-#include <vector>
 #include <numeric>
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 #define PI 3.14159265
+
+using namespace std;
 
 double targetX, targetY, prev_targetX, prev_targetY, width, height;
 double targetX_c, targetY_c, width_c, height_c;
 const int depthStepSize = 2;
 const double kinectFovX = 30.0;
 double targetPositionY = 0.0, targetPositionX = 0.0;
+double prev_targetPositionX = 0, prev_targetPositionY = 0;
+double yaw = 0;
 
 static bool detected_human = false;
 static bool prev_found = false;
@@ -46,41 +56,39 @@ long int min_vicinity;
 int fail_count = 0;
 const int max_fail_count = 10;
 
-using namespace std;
-
 class YoloPostProcessing
 {
+private:
   ros::NodeHandle nh_;
-  ros::Subscriber darknet_sub;
   ros::Publisher target_position_pub;
   ros::Publisher targetX_pub;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber depth_image_sub_;
   KalmanFilter kalman_filter_;
   tf::TransformListener tf_listener_;
   tf::StampedTransform zed_camera_center_transform_;
+  message_filters::Subscriber<sensor_msgs::Image> depth_sub;
+  message_filters::Subscriber<darknet_ros_msgs::BoundingBoxes> bb_sub;
+
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, darknet_ros_msgs::BoundingBoxes> MySyncPolicy;
+  typedef message_filters::Synchronizer<MySyncPolicy> Sync;
+  boost::shared_ptr<Sync> sync_;
 
 public:
-  YoloPostProcessing() : it_(nh_)
+  YoloPostProcessing()
   {
-    // Subscribe depth image
-    depth_image_sub_ = it_.subscribe("/zed/depth/image_raw", 1,
-                                     &YoloPostProcessing::depthCb, this);
-
-    darknet_sub = nh_.subscribe("/darknet_ros/bounding_boxes", 1,
-                                &YoloPostProcessing::msgCb, this);
-
+    depth_sub.subscribe(nh_, "/zed/depth/image_raw", 1);
+    bb_sub.subscribe(nh_, "/darknet_ros/bounding_boxes", 1);
+    sync_.reset(new Sync(MySyncPolicy(10), depth_sub, bb_sub));
+    sync_->registerCallback(boost::bind(&YoloPostProcessing::callback, this, _1, _2));
     target_position_pub =
         nh_.advertise<geometry_msgs::PoseStamped>("human_pose", 1);
     targetX_pub =
         nh_.advertise<std_msgs::Int16>("targetX", 1);
-
     wait_transform();
   }
 
-  void msgCb(const darknet_ros_msgs::BoundingBoxes::ConstPtr &msg)
+  void callback(const sensor_msgs::ImageConstPtr &depth_msg, const darknet_ros_msgs::BoundingBoxes::ConstPtr &bb_msg)
   {
-
+    // Bounding Box calculation
     if (!initialized)
     {
       initialized = true;
@@ -94,15 +102,15 @@ public:
       min_vicinity = 400000; // starts with very high number
     }
 
-    for (int i = 0; i < msg->bounding_boxes.size(); i++)
+    for (int i = 0; i < bb_msg->bounding_boxes.size(); i++)
     {
-      if (msg->bounding_boxes[i].Class == "person")
+      if (bb_msg->bounding_boxes[i].Class == "person")
       {
 
-        width_c = msg->bounding_boxes[i].xmax - msg->bounding_boxes[i].xmin;
-        targetX_c = msg->bounding_boxes[i].xmin + (width_c) / 2;
-        height_c = msg->bounding_boxes[i].ymax - msg->bounding_boxes[i].ymin;
-        targetY_c = msg->bounding_boxes[i].ymin + (height_c) / 2;
+        width_c = bb_msg->bounding_boxes[i].xmax - bb_msg->bounding_boxes[i].xmin;
+        targetX_c = bb_msg->bounding_boxes[i].xmin + (width_c) / 2;
+        height_c = bb_msg->bounding_boxes[i].ymax - bb_msg->bounding_boxes[i].ymin;
+        targetY_c = bb_msg->bounding_boxes[i].ymin + (height_c) / 2;
 
         blobArea = width_c * height_c;
 
@@ -166,29 +174,26 @@ public:
     prev_targetY = targetY;
     prev_blobArea = width * height;
     min_vicinity = 400000; // very high number
-  }
 
-  void depthCb(const sensor_msgs::ImageConstPtr &msg)
-  {
+    // Depth calculation
     cv_bridge::CvImagePtr cv_ptr;
     try
     {
-      cv_ptr = cv_bridge::toCvCopy(msg);
+      cv_ptr = cv_bridge::toCvCopy(depth_msg);
     }
     catch (cv_bridge::Exception &e)
     {
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-
     if (detected_human)
     {
       detected_human = false;
-      std::vector<float> dists;
+      vector<float> dists;
 
-      for (int i = -width / 2; i <= width / 2; i += depthStepSize)
+      for (int i = -width / 4; i <= width / 4; i += depthStepSize)
       {
-        for (int j = -height / 2; j <= height / 2;
+        for (int j = -height / 4; j <= height / 4;
              j += depthStepSize)
         {
           auto dist_ij =
@@ -204,18 +209,16 @@ public:
       {
         if (!dists.empty())
         {
-          float avg = std::accumulate(dists.begin(), dists.end(), 0.0) / dists.size();
+          float avg = accumulate(dists.begin(), dists.end(), 0.0) / dists.size();
           auto it = remove_if(dists.begin(), dists.end(), bind2nd(greater<float>(), avg + 1));
           dists.erase(it, dists.end());
         }
       }
 
-      // targetDistance /= num_values;
-
       if (!dists.empty())
       {
-        float targetDistance = std::accumulate(dists.begin(), dists.end(), 0.0) / dists.size();
-        // std::cout << "before: " << avg << "       after: " << targetDistance << std::endl;
+        float targetDistance = accumulate(dists.begin(), dists.end(), 0.0) / dists.size();
+        // cout << "before: " << avg << "       after: " << targetDistance << endl;
         targetPositionY =
             -targetDistance *
             sin((kinectFovX * ((targetX - 320) / 320.0)) * (PI / 180));
@@ -223,24 +226,37 @@ public:
             sqrt(pow(targetDistance, 2) - pow(targetPositionY, 2));
 
         Eigen::Matrix<double, kNumStates, 1> measurement;
-        std::cout << "Before: " << targetPositionX << ",   " << targetPositionY << std::endl;
+        cout << "Before: " << targetPositionX << ",   " << targetPositionY << endl;
         TransformPose(targetPositionX, targetPositionY);
-        std::cout << "After: " << targetPositionX << ",   " << targetPositionY << std::endl;
-        measurement << targetPositionX, targetPositionY, 0, 0;
-        (void)kalman_filter_.predict(ros::Time::now().toSec());
-        auto output = kalman_filter_.update(measurement);
-        //        ROS_INFO("Depth: %f , targetX: %f, targetY: %f", targetDistance,
-        //                 targetPositionX, targetPositionY);
+        cout << "After: " << targetPositionX << ",   " << targetPositionY << endl;
+
+        double y_diff = targetPositionY - prev_targetPositionY;
+        double x_diff = targetPositionX - prev_targetPositionX;
+        double total_distance = abs(y_diff) + abs(x_diff);
+        cout << "total distance: " << total_distance << endl;
+        if (total_distance > 0.4)
+        {
+          yaw = std::atan2(y_diff, x_diff);
+          prev_targetPositionX = targetPositionX;
+          prev_targetPositionY = targetPositionY;
+        }
+        // measurement << targetPositionX, targetPositionY, 0, 0;
+        //(void)kalman_filter_.predict(ros::Time::now().toSec());
+        // auto output = kalman_filter_.update(measurement);
+        //         ROS_INFO("Depth: %f , targetX: %f, targetY: %f", targetDistance,
+        //                  targetPositionX, targetPositionY);
         geometry_msgs::PoseStamped poseMsg;
         poseMsg.header.frame_id = "map";
         poseMsg.header.stamp = ros::Time::now();
-        // poseMsg.pose.position.x = targetPositionX;
-        // poseMsg.pose.position.y = targetPositionY;
-        poseMsg.pose.position.x = output(0);
-        poseMsg.pose.position.y = output(1);
-        poseMsg.pose.orientation = tf::createQuaternionMsgFromYaw(atan2(output(3), output(2)));
-        std::cout << "X: " << output(0) << " Y: " << output(1) << std::endl;
-        std::cout << "VX: " << output(2) << " VY: " << output(3) << std::endl;
+        poseMsg.pose.position.x = targetPositionX;
+        poseMsg.pose.position.y = targetPositionY;
+        // poseMsg.pose.position.x = output(0);
+        // poseMsg.pose.position.y = output(1);
+        // poseMsg.pose.orientation = tf::createQuaternionMsgFromYaw(atan2(output(3), output(2)));
+        poseMsg.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
+        // cout << "X: " << output(0) << " Y: " << output(1) << endl;
+        // cout << "VX: " << output(2) << " VY: " << output(3) << endl
+        //      << endl;
 
         target_position_pub.publish(poseMsg);
         prev_found = true;
@@ -256,14 +272,35 @@ public:
       poseMsg.pose.position.x = targetPositionX;
       poseMsg.pose.position.y = targetPositionY;
 
-      auto yaw = atan2(output(3), output(2));
+      // auto yaw = atan2(output(3), output(2));
       poseMsg.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
-      std::cout << "X: " << output(0) << " Y: " << output(1) << std::endl;
-      std::cout << "VX: " << output(2) << " VY: " << output(3) << std::endl;
+      // std::cout << "X: " << output(0) << " Y: " << output(1) << std::endl;
+      // std::cout << "VX: " << output(2) << " VY: " << output(3) << std::endl;
 
       target_position_pub.publish(poseMsg);
     }
   }
+  void mainLoop()
+  {
+    if (!prev_found)
+    {
+      auto output = kalman_filter_.predict(ros::Time::now().toSec());
+      geometry_msgs::PoseStamped poseMsg;
+      poseMsg.header.frame_id = "map";
+      poseMsg.header.stamp = ros::Time::now();
+      poseMsg.pose.position.x = targetPositionX;
+      poseMsg.pose.position.y = targetPositionY;
+
+      // auto yaw = atan2(output(3), output(2));
+      poseMsg.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
+      // cout << "X: " << output(0) << " Y: " << output(1) << endl;
+      // cout << "VX: " << output(2) << " VY: " << output(3) << endl;
+
+      target_position_pub.publish(poseMsg);
+    }
+    prev_found = false;
+  }
+
   void wait_transform()
   {
     while (true)
@@ -298,12 +335,13 @@ public:
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "yolo_pp");
-  ros::NodeHandle nh;
+  YoloPostProcessing yolo_pp;
   cout << "Post processing module started!" << endl;
   ros::Rate rate(10);
-  YoloPostProcessing yolo_pp;
+
   while (ros::ok())
   {
+    yolo_pp.mainLoop();
     ros::spinOnce();
     rate.sleep();
   }
